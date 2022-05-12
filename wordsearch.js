1b167a0bbf8bfe20bb724ba18f25dc84affe78b0
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-11 19:39:23 -0700

    answer

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// make one horizonJoin function return arry of string
+//make one verticalJoin function return arry of string
+// use the wordSearch check array
 
-module.exports = wordSearch
+const verJoin = function (matrix) {
+  let newArr = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    let subArr = [];
+    for (let j = 0; j < matrix.length; j++) {
+      subArr.push(matrix[j][i]);
+    }
+    newArr.push(subArr.join(''));
+  }
+
+  return newArr;
+};
+
+const wordSearch = (letters, word) => {
+  word = word.toUpperCase();
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  // console.log(horizontalJoin);
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  const verArr = verJoin(letters);
+  for (let i in verArr) {
+    if (verArr[i].includes(word)) return true;
+  }
+  return false;
+};
+module.exports = wordSearch;
